133ed611bb08e24707a2f59b62bef7ed28dcd56e
Remix Workspace <>
2022-08-08 20:00:38 +0530

    coded some variables into smart contract file

---
+++ back-end/Smart_Contract.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.8.7;
+
+contract product_database
+{
+    struct product_database
+    {
+        string product_name;
+        uint product_id;
+        string manufacturer_name;
+        uint manufacturer_id;
+    }
+    
+}
