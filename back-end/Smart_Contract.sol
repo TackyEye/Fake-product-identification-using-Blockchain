53e399a009bb5504b2d46b96166c3641b022f38f
Remix Workspace <>
2022-08-09 17:25:33 +0530

    Updated smartcontract

--- back-end/Smart_Contract.sol
+++ back-end/Smart_Contract.sol
@@ -1,13 +1,21 @@
+//SPDX-License-Identifier: UNLICENSED
+
 pragma solidity 0.8.7;
 
 contract product_database
 {
-    struct product_database
+    struct product_info
     {
         string product_name;
         uint product_id;
         string manufacturer_name;
         uint manufacturer_id;
     }
-    
+    product_info[] product_array;
+    function setProduct() public {
+      product_array[0] = product_info('Chess Board', 123, 'Chess company', 456);
+   }
+   function getProduct() public view returns (product_info) {
+      return memory product_array[0];
+   }  
 }
